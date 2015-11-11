83953d39c1cbaca2fc54f3afa826203a37e785b7
AvatarSD <sd1995@ukr.net>
2015-11-11 12:54:47 +0200

    sd not works

--- src/main.cpp
+++ src/main.cpp
@@ -41,21 +41,21 @@ ISR(USART1_TX_vect)
 void mainLoop()
 {
 	LED_ON;
-	debug(F("--------INT---------\n"));
+	debug(F("/n--------INT---------"));
 	static int ntcRefrwshPeriodCount = 0;
 	if(!ntcRefrwshPeriodCount)
 		_network->refreshTime();
 	if(++ntcRefrwshPeriodCount == (NTC_REFRESH_PERIOD*60/TIMER_PERIOD))
 		ntcRefrwshPeriodCount = 0;
 	while(!_network->sendTemp());
-	debug(F("-------INTEND-------\n"));
+	debug(F("-------INTEND-------"));
 	LED_OFF;
 }
 
 void screensaver(int time)
 {
 	char buff[50];
-	sprintf(buff, "\nTime to next cycle:\n        %2d:%02d\n\n",
+	sprintf(buff, "\nTime to next cycle:\n     %5d:%02d\n\n",
 			time/60, time%60);
 	debug(buff);
 }
@@ -82,6 +82,27 @@ ISR(TIMER1_CAPT_vect)
 	++breakCounter;
 	if((breakCounter >= TIMER_PERIOD*60)&&(!mutex))
 		breakCounter = 0;
+}
+
+
+
+
+FATFS FatFs;	// FatFs work area
+FIL *fp;		// fpe object
+
+
+/*---------------------------------------------------------*/
+/* User Provided RTC Function called by FatFs module       */
+/* Used to provide a Timestamp for SDCard files and folders*/
+DWORD get_fattime (void)
+{
+	// Returns current time packed into a DWORD variable
+	return	  ((DWORD)(2013 - 1980) << 25)	// Year 2013
+			| ((DWORD)8 << 21)				// Month 7
+			| ((DWORD)2 << 16)				// Mday 28
+			| ((DWORD)20 << 11)				// Hour 0..24
+			| ((DWORD)30 << 5)				// Min 0
+			| ((DWORD)0 >> 1);				// Sec 0
 }
 
 
@@ -138,7 +159,93 @@ int main(void)
 	ICR1L=0x23;
 
 	// Timer/Counter 1 Interrupt(s) initialization
-	TIMSK1=(1<<ICIE1) | (0<<OCIE1C) | (0<<OCIE1B) | (0<<OCIE1A) | (0<<TOIE1);
+	//TIMSK1=(1<<ICIE1) | (0<<OCIE1C) | (0<<OCIE1B) | (0<<OCIE1A) | (0<<TOIE1);
+
+	delay(500);
+
+
+
+
+
+
+
+
+
+
+
+	/*
+	FATFS FatFs;	// FatFs work area
+	FIL *fp;		// fpe object
+
+	debug(f_mount(0, &FatFs));*/
+
+	// init sdcard
+	UINT bw;
+	f_mount(0, &FatFs);		// Give a work area to the FatFs module
+	// open file
+	fp = (FIL *)malloc(sizeof (FIL));
+	FRESULT res = f_open(fp, "file.txt", FA_WRITE | FA_CREATE_ALWAYS);
+	if (res == FR_OK) {	// Create a file
+		char *text = "Hello World! SDCard support up and running!\r\n";
+		f_write(fp, text, strlen(text), &bw);	// Write data to the file
+		f_close(fp);// Close the file
+		if (bw == strlen(text)) { //we wrote the entire string
+			//led1.Set(1); // Lights LED if data written well (D4 led on atmega128 board)
+			//lcd.goto_xy(0,0); lcd.send_format_string("Created:%dB\n", f_size(fp));
+			debug(f_size(fp)); debug("\n");
+		}
+		//else led2.Set(1);
+	}
+	else{ debug("file not open\n");
+	 debug(res);}
+	// test append
+	if (f_open(fp, "file.txt", FA_WRITE | FA_OPEN_ALWAYS) == FR_OK) {	// Open existing or create new file
+		if (f_lseek(fp, f_size(fp)) == FR_OK)
+		{
+			char *text2 = "This is a new line, appended to existing file!\r\n";
+			f_write(fp, text2, strlen(text2), &bw);	// Write data to the file
+			if (bw == strlen(text2)) { //we wrote the entire string
+				//lcd.send_format_string("Appended:%dB\n", f_size(fp));
+				debug(f_size(fp)); debug("\n");
+			}
+
+		}
+		f_close(fp);// Close the file
+	}
+	char str[12];
+
+
+	// get card volume
+	char szCardLabel[12] = {0};
+	DWORD sn = 0;
+	if (f_getlabel("", szCardLabel, &sn) == FR_OK) {
+		//lcd.send_format_string("%s SN:%X\n", szCardLabel, sn);
+		debug(szCardLabel); debug("\n");
+		debug(sn); debug("\n");
+	}
+
+	// read from file
+	if (f_open(fp, "file.txt", FA_READ ) == FR_OK) {	// Create a file
+		char text[255];
+		UINT br;
+		f_read(fp, text, 255, &br);
+		f_close(fp);// Close the file
+		// cut text the easy way
+		text[10] = 0;
+		//lcd.send_format_string("Read:%s", text);
+		debug(text); debug("\n");
+	}
+
+
+
+
+
+
+
+
+
+
+
 
 	while(1)
 	{
